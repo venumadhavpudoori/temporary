e18f4d43392062af436812a214c7c3a75d79e5cd
msrikant15 <msrikant@gmail.com>
2020-01-23 16:15:39 +0530

    Instructions for next task

--- src/App.js
+++ src/App.js
@@ -61,6 +61,22 @@ class App extends Component {
        <input type="file" onChange={this.uploadFile} /><br></br><br></br><br></br><br></br>
        <button onClick={this.todoMutation}>GraphQL Mutation</button><br></br>
        <button  onClick={this.listQuery}>GraphQL Query</button><br></br><br></br>
+       <p> this is testing the graphql functionality. We need to add the functionality to add 
+         the data in the  src\data\CardType.js and send it to the appsync. the array in the json 
+         has 15 items
+         the graphql schema is given here amplify\backend\api\temp9\schema.graphql
+         and the operations are given here src\graphql
+
+         the expection is to have two very simple pieces of functionality
+         i. A dropdown which lets user choose the item for the list above, display the content of the 
+         item as plain text field (inner elements of the JSON do not have to be hamdled separately.) . 
+         and finally, an upload button which uploads the specific item to appsync
+
+         ii. a display button, which upon clicking should display all the current elements 
+
+
+
+       </p>
 
        <form>
         <label>
